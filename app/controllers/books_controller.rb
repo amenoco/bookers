fe00bbd979d0f51eaa86953269750b4b333c18ca
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-11-21 15:26:09 +0000

    index編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,22 @@
 class BooksController < ApplicationController
-  def new
-  end
+ 
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+    @book = Book.find(params[:id])  
   end
+   private
+   
+    def book_params
+    params.require(:book).permit(:title, :body)
+  end
+   
 end
