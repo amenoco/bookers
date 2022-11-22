892eed086e14c87802cd6391e6c8ee9d4d95980b
amenoco <ec2-user@ip-172-31-0-176.ap-northeast-1.compute.internal>
2022-11-22 16:32:26 +0000

    エラー表記手前まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,42 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to books_path(book.id)
+    
+  end
   def show
     @book = Book.find(params[:id])  
   end
 
   def edit
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  
+  def destroy
+    book = Book.find(params[:id])  
+    book.destroy  
+    redirect_to books_path
+  end
+
+ 
+
+  
+  
    private
    
-    def book_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
    
